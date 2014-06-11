704948c6885a3db81b00366d8291de977edbc733
unknown <derekbking@gmail.com>
2014-06-11 16:12:12 -0500

    Initial commit

---
+++ src/com/cypher/core/MedievalZ.java
@@ -0,0 +1,12 @@
+package com.cypher.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MedievalZ extends JavaPlugin {
+	
+	public static MedievalZ p;
+	
+	@Override
+	public void onEnable() {
+	}
+}
