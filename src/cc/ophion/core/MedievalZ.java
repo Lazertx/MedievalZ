f0f00f80521a05d3ae4002e360c85352048fa200
unknown <derekbking@gmail.com>
2014-06-11 19:45:51 -0500

    Added config loading

--- src/cc/ophion/core/MedievalZ.java
+++ src/cc/ophion/core/MedievalZ.java
@@ -1,12 +1,26 @@
 package cc.ophion.core;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MedievalZ extends JavaPlugin {
 	
 	public static MedievalZ p;
+	private FileConfiguration config = null;
 	
 	@Override
 	public void onEnable() {
+		p = this;
+		loadConfig();
+	}
+	
+	private void loadConfig() {
+		this.getConfig().options().copyDefaults(true);
+		this.saveConfig();
+		config = this.getConfig();
+	}
+	
+	public FileConfiguration getFileConfig() {
+		return config;
 	}
 }
